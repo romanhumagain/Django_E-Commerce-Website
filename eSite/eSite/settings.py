80fbb3b54b459f1563627188191462c627025710
Roman Humagain <127822516+romanhumagain@users.noreply.github.com>
2023-05-28 08:55:13 -0700

    Updated Codes

--- eSite/eSite/settings.py
+++ eSite/eSite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -31,14 +32,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'shop.apps.ShopConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',
-    'shop'
+    'blog'
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Managing media
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- eSite/shop/views.py
+++ eSite/shop/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import product
+from math import ceil
 # Create your views here.
 def index(request):
-  return render(request, 'shop/index.html')
+    products = product.objects.all()
+    print(products)
+    n = len(products)
+    nSlides = n//4 + ceil((n/4)-(n//4))
+    params = {'no_of_slides':nSlides, 'range': range(1,nSlides),'product': products}
+    return render(request, 'shop/index.html', params)
 
 def about(request):
-  return HttpResponse("About E-Commerce Website")
+  return render(request , 'shop/about.html')
 
 def contact(request):
   return HttpResponse("Contact Us Please for furthor information")
